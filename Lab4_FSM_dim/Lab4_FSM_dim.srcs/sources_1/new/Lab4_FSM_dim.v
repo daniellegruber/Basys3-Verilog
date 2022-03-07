effa424ccd5d3119d736f71fecfdb8b65e8d11a7
Danielle Lauren Gruber <danielle.l.gruber@gmail.com>
2022-03-07 12:39:47 -0500

    Changed files

--- Lab4_FSM_dim/Lab4_FSM_dim.srcs/sources_1/new/Lab4_FSM_dim.v
+++ Lab4_FSM_dim/Lab4_FSM_dim.srcs/sources_1/new/Lab4_FSM_dim.v
@@ -12,8 +12,12 @@ module FSM( clk, sw, led );
     parameter S3 = 4'b0111;
     parameter S4 = 4'b1111;
     
-    parameter PWM_input = 4'b0001;
-    reg [3:0] PWM; // internal accumulator
+//    parameter PWM_input = 4'b1111;
+//    reg [3:0] PWM; // internal accumulator
+
+    wire [7:0] x_in; //control value that defines pulse width
+    reg PWM_out = 1;
+    reg [7:0] counter = 0;
     
     reg [3:0] state_l, nextstate_l, state_r, nextstate_r;
         
@@ -25,12 +29,17 @@ module FSM( clk, sw, led );
     
     // counter
     reg [32:0] count1;
-    reg [32:0] count2;
 
     always @ (posedge clk_tail)
     begin
         state_l <= nextstate_l;   
-        state_r <= nextstate_r;        
+        state_r <= nextstate_r;  
+        
+        if ( counter < x_in )
+        PWM_out <= 1;
+        else
+        PWM_out <= 0;
+        counter <= counter+1;      
     end
 
 //  always @ (*) // next state logic
@@ -41,109 +50,74 @@ module FSM( clk, sw, led );
         case (state_l)
             S0:      nextstate_l <= S4;
             S4:      nextstate_l <= S0;
-            default: nextstate_l <= S0;
+        default: nextstate_l <= S0;
         endcase
         case (state_r)
             S0:      nextstate_r <= S4;
             S4:      nextstate_r <= S0;
-            default: nextstate_r <= S0;
+        default: nextstate_r <= S0;
         endcase
-    end else if (sw[0]) begin   // left signal
+    end else if (sw[1]) begin   // left signal
+        nextstate_r <= S0;
         case (state_l)
-            S0:     begin 
-                    count1 <= 0;
-                    
-                    if (count1 < 2500000) begin
-                        {state_l[0], PWM} <= PWM + PWM_input;
-                        count1 <= count1 + 1; 
-                    end
-                        
-                    nextstate_l <= S1;
-                    end
-                    
-            S1:     begin 
-                    count1 <= 0;
-                                
-                    if (count1 < 2500000) begin
-                        {state_l[1], PWM} <= PWM + PWM_input;
-                        count1 <= count1 + 1; 
-                    end
-                        
-                    nextstate_l <= S2;
-                    end
-                    
-            S2:     begin 
-                    count1 <= 0;
-                                
-                    if (count1 < 2500000) begin
-                        {state_l[2], PWM} <= PWM + PWM_input;
-                        count1 <= count1 + 1; 
-                    end
-                        
-                    nextstate_l <= S3;
-                    end
-                    
-            S3:     begin 
-                    count1 <= 0;
-                                
-                    if (count1 < 2500000) begin
-                        {state_l[3], PWM} <= PWM + PWM_input;
-                        count1 <= count1 + 1; 
-                    end
-                        
-                    nextstate_l <= S0;
-                    end
-                                
-            //default: nextstate_l = S0;
+        S0:     begin 
+                
+                nextstate_l[0] = PWM_out; 
+                nextstate_l <= S1;
+                end
+                
+        S1:     begin 
+                
+                nextstate_l[1] = PWM_out; 
+                nextstate_l <= S2;
+                end
+                
+        S2:     begin 
+                            
+                nextstate_l[2] = PWM_out; 
+                nextstate_l <= S3;
+                end
+                
+        S3:     begin 
+        
+                nextstate_l[3] = PWM_out; 
+                nextstate_l <= S0;
+                end
+                            
+        default: nextstate_l <= S0;
         endcase
-    end else if (sw[1]) begin   // hazard signal
+    end else if (sw[0]) begin   // hazard signal
+        nextstate_l <= S0;
         case (state_r)
-            S0:     begin 
-                    count1 <= 0;
-                    
-                    if (count1 < 2500000) begin
-                        {state_r[0], PWM} <= PWM + PWM_input;
-                        count1 <= count1 + 1; 
-                    end
-                        
-                    nextstate_r <= S1;
-                    end
-                    
-            S1:     begin 
-                    count1 <= 0;
-                                
-                    if (count1 < 2500000) begin
-                        {state_r[1], PWM} <= PWM + PWM_input;
-                        count1 <= count1 + 1; 
-                    end
-                        
-                    nextstate_r <= S2;
-                    end
-                    
-            S2:     begin 
-                    count1 <= 0;
-                                
-                    if (count1 < 2500000) begin
-                        {state_r[2], PWM} <= PWM + PWM_input;
-                        count1 <= count1 + 1; 
-                    end
-                        
-                    nextstate_l <= S3;
-                    end
-                    
-            S3:     begin 
-                    count1 <= 0;
-                                
-                    if (count1 < 2500000) begin
-                        {state_r[3], PWM} <= PWM + PWM_input;
-                        count1 <= count1 + 1; 
-                    end
-                        
-                    nextstate_r <= S0;
-                    end
-                                        
-                    //default: nextstate_l = S0;
+        S0:     begin 
+                
+                nextstate_r[0] = PWM_out;  
+                nextstate_r <= S1;
+                end
+                
+        S1:     begin 
+                
+                nextstate_r[1] = PWM_out;  
+                nextstate_r <= S2;
+                end
+                
+        S2:     begin 
+                
+                nextstate_r[2] = PWM_out;  
+                nextstate_r <= S3;
+                end
+                
+        S3:     begin 
+                
+                nextstate_r[3] = PWM_out;  
+                nextstate_r <= S0;
+                end
+                                    
+        default: nextstate_r <= S0;
         endcase
-    end
+        end else if (~sw[0] && ~sw[1] && ~sw[2]) begin   // hazard signal
+        nextstate_l <= 0;
+        nextstate_r <= 0;
+        end
 
 endmodule
